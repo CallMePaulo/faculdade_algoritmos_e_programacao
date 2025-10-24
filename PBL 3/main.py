38d07912dc5b59ab9b95a8402609fe27394cd40d
B.a.d.i.c.a 40 <87728189+Band40@users.noreply.github.com>
2025-10-24 09:20:14 -0300

    main.py

--- PBL 3/main.py
+++ PBL 3/main.py
@@ -1,11 +1,8 @@
-# main.py
-
-from func import * # Importa todas as funções de lógica
-
-# Loop principal
+from func import * 
 rodando = True
 
 while rodando:
     rodando = menu()
     
+
 print("Programa finalizado. Os dados não foram salvos.")
